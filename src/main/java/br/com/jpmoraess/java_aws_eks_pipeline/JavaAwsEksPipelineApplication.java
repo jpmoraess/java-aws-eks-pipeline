d138d422712e0f5acd9456a5884a1149a1c147ca
jpmoraess <moraess.it@gmail.com>
2025-04-26 08:26:31 -0300

    Initial commit

---
+++ src/main/java/br/com/jpmoraess/java_aws_eks_pipeline/JavaAwsEksPipelineApplication.java
@@ -0,0 +1,13 @@
+package br.com.jpmoraess.java_aws_eks_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAwsEksPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAwsEksPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jpmoraess/java_aws_eks_pipeline/JavaAwsEksPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jpmoraess.java_aws_eks_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAwsEksPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
